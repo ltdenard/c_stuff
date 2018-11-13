3d42ac090d668e075b3ae7daa41a99471423f6d6
Leon Denard <leon.denard@theice.com>
2018-11-13 17:36:47 -0500

    adding in more C stuff

--- seerver.c
+++ seerver.c
@@ -8,9 +8,17 @@
 #include <openssl/err.h>
 
 /*
+download openssl 1.1.1
+./config shared --prefix=/home/ldenard/openssltls13 --openssldir=/home/ldenard/openssltls13/ssl -Wl,--enable-new-dtags,-rpath=/home/ldenard/openssltls13/lib
+make
+make install
 openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out cert.pem
-gcc -lssl -lcrypto -o seerver seerver.c
-gcc seerver.c -o seerver -I/usr/local/ssl/include -L/usr/local/ssl/lib -lssl -lcrypto
+# gcc -lssl -lcrypto -o seerver seerver.c
+# gcc seerver.c -o seerver -I/usr/local/ssl/include -L/usr/local/ssl/lib -lssl -lcrypto
+export LD_LIBRARY_PATH=/home/ldenard/openssltls13/lib:$LD_LIBRARY_PATH
+gcc seerver.c -o seerver -I/home/ldenard/openssltls13/include -L/home/ldenard/openssltls13/lib -lssl -lcrypto
+
+./openssl s_client -connect 127.0.0.1:4433
 */
 int create_socket(int port)
 {
@@ -56,13 +64,13 @@ SSL_CTX *create_context()
     const SSL_METHOD *method;
     SSL_CTX *ctx;
 
-    method = SSLv23_server_method();
+    method = TLS_server_method();
 
     ctx = SSL_CTX_new(method);
     if (!ctx) {
-    perror("Unable to create SSL context");
-    ERR_print_errors_fp(stderr);
-    exit(EXIT_FAILURE);
+        perror("Unable to create SSL context");
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
     }
 
     return ctx;
@@ -70,7 +78,8 @@ SSL_CTX *create_context()
 
 void configure_context(SSL_CTX *ctx)
 {
-    SSL_CTX_set_ecdh_auto(ctx, 1);
+    /*SSL_CTX_set_ecdh_auto(ctx, 1);*/
+    SSL_CTX_set_cipher_list(ctx, "TLS13-CHACHA20-POLY1305-SHA256");
 
     /* Set the key and cert */
     if (SSL_CTX_use_certificate_file(ctx, "cert.pem", SSL_FILETYPE_PEM) <= 0) {
